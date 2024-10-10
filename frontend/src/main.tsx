c86aeb2437d207c3f374bdb0e80e4519b36b0b19
wuihee <wuihee@gmail.com>
2024-10-10 01:49:02 -0700

    Updated frontend project structure.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ImageUpload from "./ImageUpload";
 import "./index.css";
+import ImageUpload from "./pages/ImageUpload";
 
 const router = createBrowserRouter([
   {
