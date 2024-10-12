b0718e0fe3c2c743d8fad114c16fd10507546acf
wuihee <wuihee@gmail.com>
2024-10-12 15:35:07 -0700

    Added shadcn UI library.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: true,
     proxy: {
-      "/api": "http://backend:5000"
-    }
+      "/api": "http://backend:5000",
+    },
   },
-})
+});
