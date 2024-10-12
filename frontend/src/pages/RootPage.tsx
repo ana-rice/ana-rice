c3c5440b28bdaf3ef8cbace66faa1665ea091a14
wuihee <wuihee@gmail.com>
2024-10-12 15:26:51 -0700

    Removed preline.

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/layout/Navbar";
 
 const RootPage: React.FC = (): JSX.Element => {
   return (
     <div>
-      <Navbar />
       <Outlet />
     </div>
   );
