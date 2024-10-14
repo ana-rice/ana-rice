93133216a3c997eb33be59afd60d24975064274e
wuihee <wuihee@gmail.com>
2024-10-13 22:54:07 -0700

    Updated Flask app to reference app directory name instead of __name__.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from .endpoints.users import blueprint as users_blueprint
 
+db = SQLAlchemy()
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask("app")
     app.register_blueprint(users_blueprint)
     return app
