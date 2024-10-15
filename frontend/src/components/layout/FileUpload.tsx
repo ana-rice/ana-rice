c3e580786ac524557491d7d04b8f169053526d36
wuihee <wuihee@gmail.com>
2024-10-14 17:59:40 -0700

    Added FileUpload component.

---
+++ frontend/src/components/layout/FileUpload.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string(),
+});
+
+const ProfileForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return <Form {...form}></Form>;
+};
+
+export { ProfileForm };
